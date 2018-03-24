6759e458a6d39310414682e8af81641742a1c847
Dan Salmon <daniel.salmon@mnsu.edu>
2018-03-24 15:17:30 +0000

    Move from SQL migration to rails

--- db/schema.rb
+++ db/schema.rb
@@ -11,59 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323204417) do
+ActiveRecord::Schema.define(version: 20180324145816) do
 
-  create_table "ALBUM", primary_key: "ALBUM_ID", force: :cascade do |t|
-    t.date    "RELEASE_DATE"
-    t.string  "LABEL"
-    t.string  "TOTAL_TIME"
-    t.integer "GENRE_ID"
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
-
-  add_index "ALBUM", ["ALBUM_ID"], name: "album_album_id_uindex", unique: true
-
-  create_table "ARTIST", primary_key: "ARTIST_ID", force: :cascade do |t|
-    t.string "NAME"
-  end
-
-  add_index "ARTIST", ["ARTIST_ID"], name: "artist_artist_id_uindex", unique: true
-
-  create_table "ARTIST_ALBUM", id: false, force: :cascade do |t|
-    t.integer "ARTIST_ID", null: false
-    t.integer "ALBUM_ID",  null: false
-  end
-
-  add_index "ARTIST_ALBUM", ["ARTIST_ID", "ALBUM_ID"], name: "sqlite_autoindex_ARTIST_ALBUM_1", unique: true
-
-  create_table "GENRE", primary_key: "GENRE_ID", force: :cascade do |t|
-    t.string "GENRE_NAME"
-  end
-
-  add_index "GENRE", ["GENRE_ID"], name: "genre_genre_id_uindex", unique: true
-
-  create_table "RATING", primary_key: "RATING_ID", force: :cascade do |t|
-    t.integer "USER_ID"
-    t.integer "ALBUM_ID"
-    t.integer "RATING"
-  end
-
-  add_index "RATING", ["RATING_ID"], name: "rating_rating_id_uindex", unique: true
-
-  create_table "TRACK", primary_key: "TRACK_ID", force: :cascade do |t|
-    t.string  "TRACK_NAME"
-    t.integer "ALBUM_ID"
-    t.string  "DURATION"
-    t.integer "ALBUM_ORDER"
-  end
-
-  add_index "TRACK", ["TRACK_ID"], name: "track_track_id_uindex", unique: true
-
-  create_table "USER", primary_key: "USER_ID", force: :cascade do |t|
-    t.string "USER_NAME"
-    t.string "PASSWORD_HASH"
-    t.text   "BIOGRAPHY"
-  end
-
-  add_index "USER", ["USER_ID"], name: "user_user_id_uindex", unique: true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+seed_artists = [
+    {:name => 'Tigers Jaw'},
+    {:name => 'Basement'},
+    {:name => 'Balance and Composure'},
+    {:name => 'La Dispute'},
+    {:name => 'Camp Cope'},
+    {:name => 'Petal'},
+    {:name => 'Quicksand'},
+    {:name => 'Honeyblood'},
+    {:name => 'Seahaven'},
+    {:name => 'Manchester Orchestra'},
+    {:name => 'Foxing'},
+    {:name => 'Ratatat'},
+    {:name => 'Andrew Jackson Jihad'},
+    {:name => 'Adventures'},
+    {:name => 'American Football'},
+    {:name => 'Arcade Fire'},
+    {:name => 'Bad Books'},
+    {:name => 'Beastie Boys'},
+    {:name => 'Bill Withers'},
+    {:name => 'Run the Jewels'},
+    {:name => 'The Menzingers'},
+    {:name => 'Modern Baseball'},
+    {:name => 'Neutral Milk Hotel'},
+    {:name => 'Old Gray'},
+    {:name => 'Pianos Become The Teeth'},
+    {:name => 'Radiator Hospital'},
+    {:name => 'Right Away, Great Captain'},
+    {:name => 'The World is a Beautiful Place and I am No Longer
+              Afraid to Die'}
+]
+    
+seed_artists.each do |a|
+    Artist.create!(a)
+end
