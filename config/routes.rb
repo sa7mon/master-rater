da71272d4e43f9ba3a0d9b60f9e611131e8adb89
Dan Salmon <daniel.salmon@mnsu.edu>
2018-04-03 00:08:59 +0000

    Define default path

--- config/routes.rb
+++ config/routes.rb
@@ -85,4 +85,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  match '*path' => redirect('/'), via: [:get, :post]
+  
 end
