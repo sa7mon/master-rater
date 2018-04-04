fa748822e93ab01803b1ab2dc0fdb55c4c5aa671
Dan Salmon <daniel.salmon@mnsu.edu>
2018-04-04 16:32:25 +0000

    Fix if logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,12 @@ class ApplicationController < ActionController::Base
   before_filter :set_current_user
   protected
   def set_current_user
-    @current_user ||= Albumrater.where(:id => session[:user_id])
-    redirect_to login_path and return unless @current_user
+    if Albumrater.where(:id => session[:user_id]).count > 0
+      @current_user = Albumrater.where(:id => session[:user_id]).first
+      redirect_to login_path and return unless @current_user
+    end
   end
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
