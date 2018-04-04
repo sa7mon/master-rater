ff4ee027dd7db9f6e66464c5107005df72e3c678
Dan Salmon <daniel.salmon@mnsu.edu>
2018-04-04 14:20:45 +0000

    Try to add the star rating system

---
+++ app/assets/javascripts/albums.js
@@ -0,0 +1,5 @@
+// // initialize with defaults
+// $("#input-id").rating();
+
+// // with plugin options (do not attach the CSS class "rating" to your input if using this approach)
+// $("#input-id").rating({'size':'lg'});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require star-rating.min.js
 
+$(document).on('ready', function(){
+    $('#input-2').rating({
+        step: 1,
+        starCaptions: {1: 'Very Poor', 2: 'Poor', 3: 'Ok', 4: 'Good', 5: 'Very Good'},
+        starCaptionClasses: {1: 'text-danger', 2: 'text-warning', 3: 'text-info', 4: 'text-primary', 5: 'text-success'}
+    });
+});
 
 document.getElementById('artistsButton').onclick = function() {
     window.location = '/artits/'; 
