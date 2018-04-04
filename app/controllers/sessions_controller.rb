f0c19b04c2fe233b6273a38dfbfcbf2615fa4474
Dan Salmon <daniel.salmon@mnsu.edu>
2018-04-04 17:07:33 +0000

    Get Twitter flow working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,22 @@
 class SessionsController < ApplicationController
     skip_before_filter :set_current_user
     def create
-        auth=request.env["omniauth.auth"]
-        user=Albumrater.where(:provider => auth["provider"], :uid => auth["uid"]) || Albumrater.create_with_omniauth(auth)
-        session[:user_id] = user.id
+        @user = Albumrater.where(uid: auth_hash[:uid], provider: auth_hash[:provider]).first_or_create
+        session[:user_id] = @user.id
         redirect_to root_path
     end
-   
+    
     def destroy
         session.delete(:user_id)
         flash[:notice] = 'Logged out Successfully'
         redirect_to root_path
     end
+
+    protected  # Everything below this is now private. Don't put action methods down here.
+    
+    def auth_hash
+    request.env['omniauth.auth']
+    end
+
 end
         
