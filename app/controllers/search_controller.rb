318cadcb57c8fcbe8b94ca3befbaeafd0cb3ff21
Dan Salmon <daniel.salmon@mnsu.edu>
2018-04-06 19:08:32 +0000

    Remove unused search#show page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -23,39 +23,5 @@ class SearchController < ApplicationController
     seconds = seconds.to_s.rjust(2, '0')
     "#{minutes}:#{seconds}"
   end
-  
-  def show
-     @album = Album.joins(:artist, :genre).find(params[:id])
-    # @tracks = Track.where(album_id: params[:id])
-     @ratings = Rating.joins(:user).where(album_id: params[:id])
-     
-     ratingSum = 0
-     @ratings.each do |rating|
-       ratingSum += rating[:rating]
-     end
-     
-     @avgRating = ratingSum / @ratings.length
-     
-     # https://github.com/rest-client/rest-client
-     # https://musicbrainz.org/doc/Cover_Art_Archive/API#.2Frelease.2F.7Bmbid.7D.2F
-     url = "https://musicbrainz.org/ws/2/release/#{@album.musicbrainz_id}?inc=recordings&fmt=json"
-
-     baseCoverArtUrl = 'https://coverartarchive.org/release/'
-    @coverUrl = baseCoverArtUrl + @album.musicbrainz_id + "/front-500"
-    # coverArtRes = RestClient.get(@coverUrl, headers={'User-Agent' => 'Master-Rater/1.0 (salmon@protonmail.ch)'})
-
-    res = RestClient.get(url, headers={'User-Agent' => 'Master-Rater/1.0 (salmon@protonmail.ch)'})
-        
-    @jsonTracks = JSON.parse(res)["media"][0]["tracks"]
     
-    @tracks = Array.new
-    
-    @jsonTracks.each do |track|
-      # Conver duration from ms to m:ss
-      @tracks.push([track["position"], track["title"], convert_ms(track["length"].to_i)])
-    end
-    
-    
-    
-  end
 end
