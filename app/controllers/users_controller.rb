2a96653aa7170836a387df10cd56f597367a72d2
AaronDingman <aaron.dingman@mnsu.edu>
2018-04-09 05:05:35 +0000

    Added logic to handle when we do not find a user by their ID

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,13 @@ class UsersController < ApplicationController
 
   def show
     @user = Albumrater.where(nickname: params[:id]).first
-    @image = @user.image.sub!("_normal", "_200x200").sub!("http://", "https://")
-    # http://pbs.twimg.com/profile_images/671043988489539584/eel_irZJ_normal.jpg
-    # https://pbs.twimg.com/profile_images/671043988489539584/eel_irZJ_400x400.jpg
-    
-    @ratings = Rating.joins(:albumrater, :album).where("albumraters.nickname = ?", params[:id]).order(created_at: :desc)
-    @ratings2 = Rating.joins(:albumrater, :album).where("albumraters.nickname = ?", params[:id]).order(rating: :desc)
+    if @user != nil
+      @image = @user.image.sub!("_normal", "_200x200").sub!("http://", "https://")
+      # http://pbs.twimg.com/profile_images/671043988489539584/eel_irZJ_normal.jpg
+      # https://pbs.twimg.com/profile_images/671043988489539584/eel_irZJ_400x400.jpg
+      
+      @ratings = Rating.joins(:albumrater, :album).where("albumraters.nickname = ?", params[:id]).order(created_at: :desc)
+      @ratings2 = Rating.joins(:albumrater, :album).where("albumraters.nickname = ?", params[:id]).order(rating: :desc)
+    end
   end
 end
