376d0979e8fde74ae8cdf784148e0cc19ea0436e
Dan Salmon <daniel.salmon@mnsu.edu>
2018-04-10 21:02:02 +0000

    Get flash and redirect working

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,7 +1,10 @@
 class RatingsController < ApplicationController
    
    def create
-   
-   
+      
+      album_id = params[:album_id]
+      
+      flash[:notice] = "Hello from controller!"
+      redirect_to album_url(album_id)
    end
 end
