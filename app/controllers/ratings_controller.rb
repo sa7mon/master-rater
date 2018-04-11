a39b5b352bd2fd57e54d0461ff3b0ae59a4e010a
Dan Salmon <daniel.salmon@mnsu.edu>
2018-04-11 14:10:41 +0000

    Get ratings working kinda

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,9 +2,38 @@ class RatingsController < ApplicationController
    
    def create
       
+      # A naive user logged in check
+      if session[:user_id] != nil
+         logged_in = true
+      else
+         logged_in = false
+         flash[:notice] = "Error: You must be logged in to rate albums"
+         redirect_to album_url(album_id)
+      end
+      
+      # User is logged in by this point
+      
       album_id = params[:album_id]
       
-      flash[:notice] = "Hello from controller!"
+      # Either create a new rating or grab the first one
+      ratingRecord = Rating.where(:album_id => album_id, 
+                           :albumrater_id => session[:user_id])
+      
+      if ratingRecord.empty?
+         # This user hasn't rated this album yet.
+         # notice = "User hasn't rated this album yet!"
+         
+         ratingRecord = Rating.where(:album_id => album_id, 
+                           :albumrater_id => session[:user_id],
+                           :rating => params[:rating]).create
+         notice = "Created album rating successfully!"
+      else
+         rating = ratingRecord.first
+         rating.update(:rating => params[:rating])
+         notice = "Updated album rating successfully!"
+      end
+      
+      flash[:notice] = notice
       redirect_to album_url(album_id)
    end
 end
