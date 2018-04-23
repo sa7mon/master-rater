cee25b5be88b814c43a31ad7ab2390229b90031e
Dan Salmon <daniel.salmon@mnsu.edu>
2018-04-23 22:58:27 +0000

    Add test for album list page

---
+++ features/list_albums.feature
@@ -0,0 +1,8 @@
+Feature: View the All Albums page
+As a user
+I want to view all albums on the site
+So that I can discover more music
+
+Scenario: User sees all albums on the albums list page
+When I go to the albums list page
+Then I should see all albums ascending
--- features/step_definitions/music_steps.rb
+++ features/step_definitions/music_steps.rb
@@ -2,6 +2,10 @@ When(/^I go to the homepage$/) do
   visit root_path
 end
 
+When(/^I go to the albums list page$/) do
+  visit albums_path
+end
+
 Then(/^I should see recently rated albums$/) do
     assert page.has_content?("Recent Ratings")
 end
@@ -9,3 +13,15 @@ end
 Then(/^I should see recently added albums$/) do
     assert page.has_content?("Recent Albums")
 end
+
+Then(/^I should see all albums ascending$/) do
+    albums = Album.joins(:artist, :genre).order('name ASC')
+    
+    albums.each do |album|
+        assert page.has_link?(album)
+    end
+end
+
+
+
+
