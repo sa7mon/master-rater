cd7a7bb994ba5b9ace5481c1c9de5212b2defe87
Dan Salmon <daniel.salmon@mnsu.edu>
2018-04-24 21:01:36 +0000

    Try to add tests for ratings

--- features/step_definitions/music_steps.rb
+++ features/step_definitions/music_steps.rb
@@ -1,3 +1,14 @@
+When(/^I go to the album page for "(.*)"$/) do |albumName|
+    # albumObject = Album.where(:name => "Run The Jewels 3")
+    albumObject = Album.all
+    puts albumObject.count
+    visit album_path(Album.where(:name => albumName).first.id)
+    
+    assert_equal(current_url, "http://www.example.com/albums/1")
+    print page.html
+    assert page.has_content?("Run The Jewels 3")
+end
+
 When(/^I go to the homepage$/) do
   visit root_path
 end
@@ -24,4 +35,17 @@ end
 
 
 
-
+Then(/^I should see a rating of "(.*)" from "(.*)"$/) do |rating, rater|
+   found = false
+   
+   rows = find_by_id('#ratings-table').all('tr')
+   
+   row.each do |row| 
+        if row.has_text(rating) and row.has_text(rater)
+            found = true
+            break
+        end
+   end
+   
+   assert found = true
+end
